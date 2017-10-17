8a3148ad173101f5f9f0fc7c445194c8bac14c02
Daniil <danilcaller@gmail.com>
2017-10-17 20:26:12 +0300

    Board
    Добавлена игра. Находится в файле Board-02

--- www/js/vue-main.js
+++ www/js/vue-main.js
@@ -20,4 +20,106 @@ var dropdown = new Vue({
                profile_history: false
             }
         })
+var full_width_game = new Vue({
+    el: ".full-width-game",
+    data: {
+        popup_share: false,
+        mes: 'Этап',
+        isActive: true,
+        isActive2: false,
+        isActive3: false,
+        isActive4: false,
+        step1: true,
+        step2: false,
+        step3: false,
+        step4: false,
+        step22: false,
+        zoom1: false,
+        zoom22: false,
+        zoom3: false,
+        zoom4: false
+    },
+    methods: {
+        close_popup_share: function() {
+            this.popup_share = false
+        },
+        open_popup_share: function() {
+            this.popup_share = true
+        },
+        zom1: function() {
+            this.zoom1 = true
+        },
+        zom22: function() {
+            this.zoom22 = true
+        },
+        zom3: function() {
+            this.zoom3 = true
+        },
+        zom4: function() {
+            this.zoom4 = true
+        }
 
+        /*,
+        mess: function() {
+            this.isActive = true,
+            this.isActive2 = false,
+            this.isActive3 = false,
+            this.isActive4 = false,
+            this.mes = 'Этап',
+            this.mes2 = '',
+            this.mes3 = '',
+            this.mes4 = '',
+            this.step1 = true,
+            this.step2 = false,
+            this.step22 = false,
+            this.step3 = false,
+            this.step4 = false
+        },
+        mess2: function() {
+            this.isActive = false,
+            this.isActive2 = true,
+            this.isActive3 = false,
+            this.isActive4 = false,
+            this.mes = '',
+            this.mes2 = 'Этап',
+            this.mes3 = '',
+            this.mes4 = '',
+            this.step1 = false,
+            this.step2 = true,
+            this.step22 = false,
+            this.step3 = false,
+            this.step4 = false
+        },
+        mess3: function() {
+            this.isActive = false,
+            this.isActive2 = false,
+            this.isActive3 = true,
+            this.isActive4 = false,
+            this.mes = '',
+            this.mes2 = '',
+            this.mes3 = 'Этап',
+            this.mes4 = '',
+            this.step1 = false,
+            this.step2 = false,
+            this.step22 = false,
+            this.step3 = true,
+            this.step4 = false
+        },
+        mess4: function() {
+            this.isActive = false,
+            this.isActive2 = false,
+            this.isActive3 = false,
+            this.isActive4 = true,
+            this.mes = '',
+            this.mes2 = '',
+            this.mes3 = '',
+            this.mes4 = 'Этап',
+            this.step1 = false,
+            this.step2 = false,
+            this.step22 = false,
+            this.step3 = false,
+            this.step4 = true
+        }*/
+
+    }
+})
